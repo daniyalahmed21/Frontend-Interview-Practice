c2e42f1aa824705346ea5c42df89fe965cf42ae7
Daniyal Ahmed <da9033431@gmail.com>
2025-09-15 19:05:53 +0100

    Custom Cursor

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CustomCursor from "./components/Custom Cursor/CustomCursor";
+
+function App() {
+  return (
+    <div>
+      <CustomCursor />;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Custom Cursor/CustomCursor.jsx
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+import "./customCursor.css";
+
+const CustomCursor = () => {
+  const circleRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    if (circleRef.current) {
+      circleRef.current.style.left = `${e.clientX}px`;
+      circleRef.current.style.top = `${e.clientY}px`;
+    }
+  };
+
+  return (
+    <div className="h-screen" onMouseMove={handleMouseMove}>
+      <div className="cursor-circle" ref={circleRef}></div>
+    </div>
+  );
+};
+
+export default CustomCursor;
